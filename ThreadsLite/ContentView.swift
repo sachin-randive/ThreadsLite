7f2db377b1867ab8151682d3775204b773b569f6
Sachin Randive <r.sachin81@gmail.com>
2025-11-28 22:35:12 +0530

    Initial commit of existing project

---
+++ ThreadsLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsLite
+//
+//  Created by Sachin Randive on 28/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsLite/ThreadsLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsLiteApp.swift
+//  ThreadsLite
+//
+//  Created by Sachin Randive on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
