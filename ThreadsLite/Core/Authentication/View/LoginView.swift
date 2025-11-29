a6a51a1b7c44e55675493b3a989c5af3f08469f0
Sachin Randive <r.sachin81@gmail.com>
2025-11-29 22:10:46 +0530

    Added registration page view

--- ThreadsLite/Core/Authentication/View/LoginView.swift
+++ ThreadsLite/Core/Authentication/View/LoginView.swift
@@ -11,69 +11,63 @@ struct LoginView: View {
     @State var email = ""
     @State var password = ""
     var body: some View {
-       NavigationStack {
-           VStack {
-               Spacer()
-               // logo image
-               Image("threads-app-icon")
-                   .resizable()
-                   .scaledToFit()
-                   .frame(width: 120, height: 120)
-                   .padding()
-               
-               VStack(spacing: 15) {
-                   TextField("Enter your email", text: $email)
-                       .font(.subheadline)
-                       .padding(12)
-                       .background(Color(.systemGray6))
-                       .cornerRadius(10)
-                       .padding(.horizontal, 24)
-                   
-                   SecureField("Enter your password", text: $password)
-                       .font(.subheadline)
-                       .padding(12)
-                       .background(Color(.systemGray6))
-                       .cornerRadius(10)
-                       .padding(.horizontal, 24)
-               }
-               // forgot password
-               NavigationLink() {
-                   Text("Forgot Password")
-               } label: {
-                   Text("Forgot Password ?")
-                       .font(.footnote)
-                       .fontWeight(.semibold)
-                       .padding(.top)
-                       .foregroundColor(.black)
-                       .frame(maxWidth: .infinity, alignment: .trailing)
-                       .padding(.trailing, 28)
-                       .padding(.bottom)
-               }
-               NavigationLink() {
-                   Text("Login")
-               } label: {
-                   Text("Login")
-                       .font(.subheadline)
-                       .fontWeight(.semibold)
-                       .foregroundColor(.white)
-                       .frame(width: 350, height: 44)
-                       .background(Color.black)
-                       .cornerRadius(8)
-               }
-               Spacer()
-               Divider()
-               NavigationLink() {
-                   Text("sign up")
-               } label: {
-                   Text("Don't have an account ? Sign Up")
-                       .font(.footnote)
-                       .fontWeight(.semibold)
-                       .padding(.top)
-                       .foregroundColor(.black)
-                       .frame(maxWidth: .infinity, alignment: .init(horizontal: .center, vertical: .top))
-               }
-               
-           }
+        NavigationStack {
+            VStack {
+                Spacer()
+                // logo image
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack(spacing: 15) {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .modifier(ThreadsTextFieldModifier())
+                    
+                    SecureField("Enter your password", text: $password)
+                        .modifier(ThreadsTextFieldModifier())
+                }
+                // forgot password
+                NavigationLink() {
+                    Text("Forgot Password")
+                } label: {
+                    Text("Forgot Password ?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 28)
+                        .padding(.bottom)
+                }
+                Button {
+                    //action
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 350, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                Spacer()
+                Divider()
+                NavigationLink() {
+                    RegistrationView()
+                } label: {
+                    HStack(spacing: 5) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 15)
+            }
         }
     }
 }
---
+++ ThreadsLite/Core/Authentication/View/RegistrationView.swift
@@ -0,0 +1,71 @@
+//
+//  RegistrationView.swift
+//  ThreadsLite
+//
+//  Created by Sachin Randive on 29/11/25.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State var email = ""
+    @State var password = ""
+    @State var fullname = ""
+    @State var username = ""
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            // logo image
+            Image("threads-app-icon")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 120, height: 120)
+                .padding()
+            
+            VStack(spacing: 15) {
+                TextField("Enter your email", text: $email)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                SecureField("Enter your password", text: $password)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                TextField("Enter your full aame", text: $fullname)
+                    .modifier(ThreadsTextFieldModifier())
+                
+                TextField("Enter your username", text: $username)
+                    .modifier(ThreadsTextFieldModifier())
+            }
+            Button {
+                //action
+            } label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 350, height: 44)
+                    .background(.black)
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            Spacer()
+            Divider()
+            Button {
+                
+            } label: {
+                HStack(spacing: 5) {
+                    Text("Already have an account?")
+                    Text("Sign In")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.black)
+                .font(.footnote)
+            }
+            .padding(.vertical, 15)
+        }
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
